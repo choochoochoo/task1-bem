56f98d8a6e918a43e868166547d233842b8217ad
choochoocho <choochocho@outlook.com>
2016-08-01 20:50:48 +0300

    make semantic tag for blocks and elements

--- desktop.blocks/section/section.bemhtml.js
+++ desktop.blocks/section/section.bemhtml.js
@@ -1,3 +1,13 @@
-block('section').elem('dropcap')(
-    tag()('span')
+block('section')(
+
+    tag()('section'),
+
+    elem('dropcap').tag()('span'),
+
+    elem('quote').tag()('blockquote'),
+
+    elem('paragraph').tag()('p'),
+
+    elem('title').tag()('h1')
+
 );
