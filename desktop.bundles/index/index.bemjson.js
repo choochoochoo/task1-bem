2a85c2e6777f7299794977778c1d2a4f1db596ac
DSSolovev <choochoochoo@outlook.com>
2016-08-11 13:48:24 +0300

    fix index.bemjosn and add description

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -3,9 +3,9 @@ module.exports = {
     title: 'ФАРЕРЫ',
     favicon: '/favicon1.ico',
     head: [
-        { elem: 'meta', attrs: { name: 'description', content: '' } },
+        { elem: 'meta', attrs: { name: 'description', content: 'Фареры' } },
         { elem: 'meta', attrs: { name: 'viewport', content: 'width=device-width, initial-scale=1' } },
-        { elem: 'meta', attrs: { name: 'robots', content='noindex' } },
+        { elem: 'meta', attrs: { name: 'robots', content: 'noindex' } },
         { elem: 'css', url: 'index.min.css' }
     ],
     scripts: [{ elem: 'js', url: 'index.min.js' }]
